f46cc331cbfe7fe9fca0ba5db18d837023243a91
ODEF <103530614+ODEF@users.noreply.github.com>
2023-05-14 09:40:42 +0400

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+text = "This morning "+ "name1" +" woke up feeling "+"adjective1"+ ". 'It is going to be a "+"adjective2"+" day!' Outside, a bunch of"+"noun1"+ "s were protesting to keep "+"noun2"+" in stores. They began to "+"verb1"+ " to the rhythm of the "+"noun3"+", which made all the "+"noun4"+"s very "+"adjective3"+". Concerned, "+"name1"+" texted "+"name2"+", who flew "+"name1"+" to "+"place1"+" and dropped "+"name1"+" in a puddle of frozen "+"noun5"+". "+"name1"+" woke up in the year "+"numbernum"+", in a world where "+"noun6"+"s ruled the world." 
+
+print(text)
+name1 = input("Name: ")
+name2 = input("Name: ")
+
+adjective1 = input("Adjective 1: ")
+adjective2 = input("Adjective 2: ")
+adjective3 = input("Adjective 3: ")
+
+noun1 = input("noun 1: ")
+noun2 = input("noun 2: ")
+noun3 = input("noun 3: ")
+noun4 = input("noun 4: ")
+noun5 = input("noun 5: ")
+noun6 = input("noun 6: ")
+
+verb1 = input("verb 1: ")
+
+place1 = input("place: ")
+
+numbernum = input("number: ")
+storys = "This morning "+ name1 +" woke up feeling "+adjective1+ ". 'It is going to be a "+adjective2+" day!' Outside, a bunch of"+noun1+ "s were protesting to keep "+noun2+" in stores. They began to "+verb1+ " to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+numbernum+", in a world where "+noun6+"s ruled the world."
+
+print(storys)
